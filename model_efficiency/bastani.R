3245ea9d154bc6906bdc1f287ba246bf37f13bdb
squaregoldfish <steve@squaregoldfish.co.uk>
2017-10-21 21:53:30 +0200

    2.5° version

--- model_efficiency/bastani.R
+++ model_efficiency/bastani.R
@@ -1,21 +1,7 @@
 # Calculate the model efficiency on a 1° version
 # using Bastani's code (converted from Matlab)
 
-# Currently restricted to the North Atlantic for comparison
-
 library(ncdf4)
-
-# Pre-made 1° biome 9 mask
-cat("\rLoading biomes      ")
-nc <- nc_open("mean_biomes_fix.nc")
-biome10 <- ncvar_get(nc, "MEANBIOMES")
-nc_close(nc)
-
-cat("\rPreprocessing biomes      ")
-
-# Clear out everything except Biome 10
-biome10[biome10 != 9] <- NA
-biome10[is.nan(biome10)] <- NA
 
 # Load SOCAT data for biome 10
 cat("\rLoading SOCAT            ")
@@ -24,30 +10,24 @@ socat <- ncvar_get(nc, "fco2")
 socat_times <- ncvar_get(nc, "time")
 nc_close(nc)
 
-for (i in 1:length(socat_times)) {
-	step_data <- socat[,,i]
-	step_data[is.na(biome10)] <- NA
-	socat[,,i] <- step_data
-}
-
 # Mean of SOCAT observations
 cat("\rPreprocessing SOCAT      ")
 socat_mean <- apply(socat, c(1,2), mean, na.rm=TRUE)
 
 # Load the interpolation output
 cat("\rLoading interpolated fCO₂      ")
-nc <- nc_open("fco2_1x1.nc")
+nc <- nc_open("fco2.nc")
 interp <- ncvar_get(nc, "fco2")
 nc_close(nc)
 
 # Calculate the efficiency
-efficiency <- vector(mode="numeric", length=360*180*372)
-dim(efficiency) <- c(360, 180, 372)
+efficiency <- vector(mode="numeric", length=144*72*372)
+dim(efficiency) <- c(144, 72, 372)
 efficiency[efficiency == 0] <- NA
 
-for (i in 1:360) {
+for (i in 1:144) {
 	cat("\rCalculating efficiency", i, "     ")
-	for (j in 1:180) {
+	for (j in 1:72) {
 
 		socat_interp_diff_sum <- 0
 		socat_mean_diff_sum <- 0
@@ -66,8 +46,8 @@ for (i in 1:360) {
 }
 
 cat("\rWriting output            ")
-lon_dim <- ncdim_def("lon", "degrees_east", seq(0.5, 359.5, 1))
-lat_dim <- ncdim_def("lat", "degrees_north", seq(-89.5, 89.5, 1))
+lon_dim <- ncdim_def("lon", "degrees_east", seq(1.25, 358.75, 2.5))
+lat_dim <- ncdim_def("lat", "degrees_north", seq(-88.75, 88.75, 2.5))
 time_dim <- ncdim_def("time", "year", socat_times)
 
 efficiency_var <- ncvar_def("efficiency", "", list(lon_dim, lat_dim, time_dim), -1e35, prec="double")
