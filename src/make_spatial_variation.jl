4af4e786f14985824fd8b86e0f9e40b83eaf5e58
Steve Jones <steve@squaregoldfish.co.uk>
2018-10-10 14:21:11 +0100

    (julia) locals and inbounds

--- src/make_spatial_variation.jl
+++ src/make_spatial_variation.jl
@@ -6,39 +6,39 @@ const OUTFILE = "pco2_spatial_variation.jldata"
 
 function run()
 	print("Loading input data...")
-	lons = Dataset(INFILE)["longitude"][:]
-	lats = Dataset(INFILE)["latitude"][:]
-	pco2 = Dataset(INFILE)["pCO2"][:,:,:]
+	local lons::Array{Union{Missing, Float32},1} = Dataset(INFILE)["longitude"][:]
+	local lats::Array{Union{Missing, Float32},1} = Dataset(INFILE)["latitude"][:]
+	local pco2::Array{Union{Missing, Float64}, 3} = Dataset(INFILE)["pCO2"][:,:,:]
 
-	lonsize::Int64 = length(lons)
-	latsize::Int64 = length(lats)
-	timesize::Int64 = size(pco2)[3]
+	local lonsize::Int64 = length(lons)
+	local latsize::Int64 = length(lats)
+	local timesize::Int64 = size(pco2)[3]
 
 	print("\033[1K\rInitialising data structures...")
-	variationtotal::Array{Float64, 4} = zeros(lonsize, latsize, lonsize, latsize)
-	variationcount::Array{Int64, 4} = zeros(lonsize, latsize, lonsize, latsize)
+	local variationtotal::Array{Float64, 4} = zeros(lonsize, latsize, lonsize, latsize)
+	local variationcount::Array{Int64, 4} = zeros(lonsize, latsize, lonsize, latsize)
 
 	for timeloop::Int64 in 1:1000
 		print("\033[1K\r$timeloop / $timesize")
 		for lonloop::Int64 in 1:lonsize
 			for latloop::Int64 in 1:latsize
-				currentvalue::Union{Missing, Float64} = pco2[lonloop, latloop, timeloop]
+				@inbounds local currentvalue::Union{Missing, Float64} = pco2[lonloop, latloop, timeloop]
 				if !ismissing(currentvalue)
 
 	                # Search for other cells with values at the same time.
 	                # Note that we include values from a 7 timesteps either side too,
 	                # just to increase the amount of data we can use. It should also
 	                # temper the most optimistic change values we see.
-	                compare::Array{Union{Missing, Float64}, 3} = pco2[:, :, (timeloop - 7):(timeloop + 7)]
-	                diffs::Array{Union{Missing, Float64}, 3} = similar(compare)
-	                @. diffs = abs(compare - currentvalue)
+	                local compare::Array{Union{Missing, Float64}, 3} = pco2[:, :, (timeloop - 7):(timeloop + 7)]
+	                local diffs::Array{Union{Missing, Float64}, 3} = similar(compare)
+	                @inbounds @. diffs = abs(compare - currentvalue)
 
 	                for difflon in 1:lonsize
 	                	for difflat in 1:latsize
-	                		celldiffs = diffs[difflon, difflat, :]
-	                		variationtotal[lonloop, latloop, difflon, difflat] = 
+	                		local celldiffs = diffs[difflon, difflat, :]
+	                		@inbounds variationtotal[lonloop, latloop, difflon, difflat] = 
 	                			variationtotal[lonloop, latloop, difflon, difflat] + sum(skipmissing(celldiffs))
-	                		variationcount[lonloop, latloop, difflon, difflat] =
+	                		@inbounds variationcount[lonloop, latloop, difflon, difflat] =
 	                			variationcount[lonloop, latloop, difflon, difflat] + sum(@. count(!ismissing(celldiffs)))
 	                	end
 	                end
@@ -48,11 +48,11 @@ function run()
 	end
 
 	print("\033[1K\rCalculating means...")
-	meanvar::Array{Float64, 4} = similar(variationtotal)
-	meanvar = variationtotal ./ variationcount
-	meanvar = replace(meanvar, NaN=>-1e35)
+	local meanvar::Array{Float64, 4} = similar(variationtotal)
+	@inbounds meanvar = variationtotal ./ variationcount
+	@inbounds meanvar = replace(meanvar, NaN=>-1e35)
 
-	out::IOStream = open(OUTFILE, "w")
+	local out::IOStream = open(OUTFILE, "w")
 	serialize(out, meanvar)
 	close(out)
 
