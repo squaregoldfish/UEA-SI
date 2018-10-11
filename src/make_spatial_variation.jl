47213632b2403e328f7f62cd8ba227df120c2f6e
Steve Jones <steve@squaregoldfish.co.uk>
2018-10-11 18:08:41 +0100

    (julia) Optimisations

--- src/make_spatial_variation.jl
+++ src/make_spatial_variation.jl
@@ -18,10 +18,22 @@ function run()
 	local variationtotal::Array{Float64, 4} = zeros(lonsize, latsize, lonsize, latsize)
 	local variationcount::Array{Int64, 4} = zeros(lonsize, latsize, lonsize, latsize)
 
-	for timeloop::Int64 in 1:1000
-		print("\033[1K\r$timeloop / $timesize")
+	local processedcount::Int64 = 0
+
+	Threads.@threads for timeloop::Int64 in 1:timesize
+		local timemin::Int64 = timeloop - 7
+		if timemin < 1
+			timemin = 1
+		end
+
+		local timemax::Int64 = timeloop + 7
+		if timemax > timesize
+			timemax = timesize
+		end
+
 		for lonloop::Int64 in 1:lonsize
 			for latloop::Int64 in 1:latsize
+
 				@inbounds local currentvalue::Union{Missing, Float64} = pco2[lonloop, latloop, timeloop]
 				if !ismissing(currentvalue)
 
@@ -29,21 +41,27 @@ function run()
 	                # Note that we include values from a 7 timesteps either side too,
 	                # just to increase the amount of data we can use. It should also
 	                # temper the most optimistic change values we see.
-	                local compare::Array{Union{Missing, Float64}, 3} = pco2[:, :, (timeloop - 7):(timeloop + 7)]
+	                local compare::Array{Union{Missing, Float64}, 3} = pco2[:, :, timemin:timemax]
 	                local diffs::Array{Union{Missing, Float64}, 3} = similar(compare)
 	                @inbounds @. diffs = abs(compare - currentvalue)
-
 	                for difflon in 1:lonsize
 	                	for difflat in 1:latsize
 	                		local celldiffs = diffs[difflon, difflat, :]
-	                		@inbounds variationtotal[lonloop, latloop, difflon, difflat] = 
-	                			variationtotal[lonloop, latloop, difflon, difflat] + sum(skipmissing(celldiffs))
-	                		@inbounds variationcount[lonloop, latloop, difflon, difflat] =
-	                			variationcount[lonloop, latloop, difflon, difflat] + sum(@. count(!ismissing(celldiffs)))
+                			@sync begin
+	                			@inbounds variationtotal[lonloop, latloop, difflon, difflat] =
+	                				variationtotal[lonloop, latloop, difflon, difflat] + sum(skipmissing(celldiffs))
+	                			@inbounds variationcount[lonloop, latloop, difflon, difflat] =
+	                				variationcount[lonloop, latloop, difflon, difflat] + sum(@. count(!ismissing(celldiffs)))
+	                		end
 	                	end
 	                end
 				end
 			end
+		end
+
+		@sync begin
+		    processedcount = processedcount + 1
+			Core.print("\033[1K\rCalculating spatial variation $processedcount / $timesize")
 		end
 	end
 
@@ -56,6 +74,7 @@ function run()
 	serialize(out, meanvar)
 	close(out)
 
+	print("\n$(length(meanvar)) $(count(meanvar .< 0))")
 	print("\n")
 end
 
